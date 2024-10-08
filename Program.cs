4c28ba1086d2adcb9bee387b54f4f33f44371231
Ryan-Haug-Student <ryha601@student.oaisd.org>
2024-10-08 21:03:47 +0000

    changed formatting

--- Program.cs
+++ Program.cs
@@ -1,48 +1,62 @@
-﻿public class Program{
-    public static void Main(string[] args){
+﻿public class Program
+{
+    public static void Main(string[] args)
+    {
 
         Console.Clear();
 
         Console.WriteLine("Please enter a number: ");
         int number = int.Parse(Console.ReadLine());
 
-        switch (number){
+        switch (number)
+        {
             case 0:
                 Console.WriteLine("Your number is 0");
                 break;
+
             case 1:
                 Console.WriteLine("Your number is 1");
                 break;
+
             case 2:
                 Console.WriteLine("Your number is 2");
                 break;
+
             case 3:
                 Console.WriteLine("Your number is 3");
                 break;
+
             case 4:
                 Console.WriteLine("Your number is 4");
                 break;
+
             case 5:
                 Console.WriteLine("Your number is 5");
                 break;
+
             case 6:
                 Console.WriteLine("Your number is 6");
                 break;
+
             case 7:
                 Console.WriteLine("Your number is 7");
                 break;
+
             case 8:
                 Console.WriteLine("Your number is 8");
                 break;
+
             case 9:
                 Console.WriteLine("Your number is 9");
                 break;
+                
             case 10:
                 Console.WriteLine("Your number is 10");
                 break;
         }
 
-        if (number > 10){
+        if (number > 10)
+        {
             Console.WriteLine("Please enter a number less than 10");
         }
 
